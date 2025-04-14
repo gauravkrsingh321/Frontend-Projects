37405e4ddad6bb36498b596495dc62b75b9669fc
gauravkrsingh321 <gauravkrsingh40@gmail.com>
2025-04-14 10:22:03 +0530

    Title Changed

--- blogs-app/src/components/Header.jsx
+++ blogs-app/src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='w-full fixed top-0 bg-white border shadow-md py-4'>
       <header className='text-center'>
-        <h1 className='font-bold text-3xl uppercase'>Codehelp Blogs</h1>
+        <h1 className='font-bold text-3xl uppercase'>Tech Blogs</h1>
       </header>
     </div>
   )
