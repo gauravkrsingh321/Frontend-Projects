6c8b1d047d74c03e85e11bcc52ecd1b094a4cdff
gauravkrsingh321 <gauravkrsingh40@gmail.com>
2025-04-15 12:38:14 +0530

    CSS Added

--- blogs-app/src/pages/BlogPage.jsx
+++ blogs-app/src/pages/BlogPage.jsx
@@ -42,7 +42,7 @@ const BlogPage = () => {
   
 
   return (
-    <div className="max-w-[900px] relative m-auto  py-8 flex flex-col justify-center items-center gap-y-10 mt-[60px] mb-[0px]">
+    <div className="max-w-[900px] pl-10 pr-10 relative m-auto  py-8 flex flex-col justify-center items-center gap-y-10 mt-[60px] mb-[0px]">
       <Header />
       <div className="w-full">
         <button className="border-2 cursor-pointer hover:bg-gray-200 p-1 pl-2 pr-2 text-[1rem]" onClick={()=>navigate(-1)}>Back</button>
